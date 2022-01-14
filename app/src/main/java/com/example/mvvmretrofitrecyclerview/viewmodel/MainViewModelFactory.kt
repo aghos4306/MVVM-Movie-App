c448328c854739e14b861ea4bb956e3b34849b14
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-01-14 14:17:37 +0200

    fixed omitted val in Factory

--- app/src/main/java/com/example/mvvmretrofitrecyclerview/viewmodel/MainViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmretrofitrecyclerview/viewmodel/MainViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmretrofitrecyclerview.repository.MainRepository
 import java.lang.IllegalArgumentException
 
-class MainViewModelFactory constructor(repository: MainRepository): ViewModelProvider.Factory {
+class MainViewModelFactory constructor(private val repository: MainRepository): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             MainViewModel(this.repository) as T
