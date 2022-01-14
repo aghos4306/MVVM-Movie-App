108412bc6ae1423bc19e7d4c6ff3a92ab04fda47
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-01-14 13:31:44 +0200

    the viewmodel and its provider

---
+++ app/src/main/java/com/example/mvvmretrofitrecyclerview/viewmodel/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.mvvmretrofitrecyclerview.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mvvmretrofitrecyclerview.model.MovieModel
+import com.example.mvvmretrofitrecyclerview.repository.MainRepository
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+
+class MainViewModel constructor(val repository: MainRepository) : ViewModel() {
+
+    val movieList = MutableLiveData<List<MovieModel>>()
+    val errorMessage = MutableLiveData<String>()
+
+    fun getAllMovies() {
+
+        val response = repository.getAllMovies()
+        response.enqueue(object : Callback<List<MovieModel>> {
+            override fun onResponse(call: Call<List<MovieModel>>, response: Response<List<MovieModel>>) {
+                movieList.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<List<MovieModel>>, t: Throwable) {
+                errorMessage.postValue(t.message)
+            }
+        })
+    }
+
+}
